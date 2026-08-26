7c67e373c0005b8d2a0e9d859587c07e16833ba7
karan-cd <karan.tolambiya@clouddrove.com>
2026-08-26 23:23:44 +0530

    fix: updated variables

--- main.tf
+++ main.tf
@@ -24,22 +24,22 @@ resource "random_uuid" "test_guids" {
 }
 
 resource "azurerm_application_insights" "main" {
-  count                                 = var.enabled ? 1 : 0
-  name                                  = var.resource_position_prefix ? format("appsights-%s", local.name) : format("%s-appsights", local.name)
-  location                              = var.location
-  resource_group_name                   = var.resource_group_name
-  application_type                      = var.application_type
-  retention_in_days                     = var.retention_in_days
-  sampling_percentage                   = var.sampling_percentage
-  daily_data_cap_in_gb                  = var.daily_data_cap_in_gb
-  daily_data_cap_notifications_disabled = var.daily_data_cap_notifications_disabled
-  disable_ip_masking                    = var.disable_ip_masking
-  workspace_id                          = var.workspace_id
-  local_authentication_disabled         = var.local_authentication_disabled
-  internet_ingestion_enabled            = var.internet_ingestion_enabled
-  internet_query_enabled                = var.internet_query_enabled
-  force_customer_storage_for_profiler   = var.force_customer_storage_for_profiler
-  tags                                  = module.labels.tags
+  count                                = var.enabled ? 1 : 0
+  name                                 = var.resource_position_prefix ? format("appsights-%s", local.name) : format("%s-appsights", local.name)
+  location                             = var.location
+  resource_group_name                  = var.resource_group_name
+  application_type                     = var.application_type
+  retention_in_days                    = var.retention_in_days
+  sampling_percentage                  = var.sampling_percentage
+  daily_data_cap_in_gb                 = var.daily_data_cap_in_gb
+  daily_data_cap_notifications_enabled = var.daily_data_cap_notifications_enabled
+  ip_masking_enabled                   = var.ip_masking_enabled
+  workspace_id                         = var.workspace_id
+  local_authentication_enabled         = var.local_authentication_enabled
+  internet_ingestion_enabled           = var.internet_ingestion_enabled
+  internet_query_enabled               = var.internet_query_enabled
+  force_customer_storage_for_profiler  = var.force_customer_storage_for_profiler
+  tags                                 = module.labels.tags
 }
 
 resource "azurerm_application_insights_web_test" "main" {
